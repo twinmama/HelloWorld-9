63e4b82ae9c58128633a5fc1aca8c162b9f36256
twinmama <twintwinmama2525@gmail.com>
2019-01-08 13:17:32 +0900

    Initial Commit

---
+++ HelloWorld 9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld 9
+//
+//  Created by Ashiwa Mami on 2019/01/08.
+//  Copyright © 2019 twinmama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
